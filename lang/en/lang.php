bb6cda9b4e32cb9972b6c62b183d1304c5d2ac51
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-04-06 17:05:01 -0400

    Initial commit

---
+++ lang/en/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Name: lang.php
+ * Description: The English language localization file for door43register plugin.
+ *
+ * Author: Phil Hopper
+ * Date:   2014-12-31
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+//$lang['translateTitle'] = 'Translate';
+
+// localized strings for JavaScript
+// js example: var text = LANG.plugins['door43register']['translate'];
+//$lang['js']['translate'] = 'Translate';
+
